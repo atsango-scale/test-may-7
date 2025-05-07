d603feb2dcab6fcf73f06d47b323abe8317fc765
Austin Atsango <austin.atsango@scale.com>
2025-05-07 15:55:08 -0700

    Add graph_tool.py with basic Graph class

--- graph_tool.py
+++ graph_tool.py
@@ -6,30 +6,22 @@ class Graph:
         if node not in self.adj_list:
             self.adj_list[node] = []
 
-    def add_edge(self, node1, node2, weight=1):
-        # Ensure both nodes exist in the graph
-        if node1 not in self.adj_list:
-            self.add_node(node1)
-        if node2 not in self.adj_list:
-            self.add_node(node2)
-        
-        # Add the edge (and its reverse for an undirected graph)
-        # For a directed graph, only the first part would be needed.
-        # Storing as a tuple (neighbor, weight)
-        self.adj_list[node1].append((node2, weight))
-        self.adj_list[node2].append((node1, weight)) # Assuming undirected graph
+    def add_edge(self, node1, node2):
+        if node1 in self.adj_list and node2 in self.adj_list:
+            self.adj_list[node1].append(node2)
+            self.adj_list[node2].append(node1)  # For an undirected graph
+        else:
+            print("Error: One or both nodes not found in the graph.")
 
-    def display_graph(self):
-        for node in self.adj_list:
-            print(f"{node}: {self.adj_list[node]}")
+    def __str__(self):
+        return str(self.adj_list)
 
 if __name__ == '__main__':
-    # Basic test
-    g = Graph()
-    g.add_node("A")
-    g.add_node("B")
-    g.add_node("C")
-    g.add_edge("A", "B", 5)
-    g.add_edge("B", "C", 3)
-    g.add_edge("A", "C", 10)
-    g.display_graph() 
+    # Example Usage
+    graph = Graph()
+    graph.add_node("A")
+    graph.add_node("B")
+    graph.add_node("C")
+    graph.add_edge("A", "B")
+    graph.add_edge("B", "C")
+    print(graph) 
